eda2b928b00fc0d3ec7ba63732bf5ff085487e24
mehtadarshil <mehtadarshil98@gmail.com>
2025-03-10 15:07:03 +0530

    dfdsf

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "@/lib/supabase/client";
 
 export async function getSession() {
   const supabase = createClient();
