9dda833508ad4539834a3744223c9ff12c0bb3e5
mehtadarshil <mehtadarshil98@gmail.com>
2025-03-10 15:13:58 +0530

    @

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,8 +1,8 @@
 import { createServerClient } from "@supabase/ssr";
-import { type RequestCookies } from "next/dist/server/web/spec-extension/cookies";
+import { type ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/cookies";
 import { type CookieOptions } from "@supabase/ssr";
 
-export const createClient = (cookies: () => RequestCookies) => {
+export const createClient = (cookies: () => ReadonlyRequestCookies) => {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -12,9 +12,11 @@ export const createClient = (cookies: () => RequestCookies) => {
           return cookies().get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
+          // @ts-ignore - ReadonlyRequestCookies vs RequestCookies mismatch
           cookies().set(name, value, options);
         },
         remove(name: string, options: CookieOptions) {
+          // @ts-ignore - ReadonlyRequestCookies vs RequestCookies mismatch
           cookies().set(name, "", { ...options, maxAge: 0 });
         },
       },
